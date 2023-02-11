af11872fcfd892fc8f77536e796c1494318a3ecb
Manssoum Mustapha <mustapha.manssoum@telecom-st-etienne.fr>
2023-02-11 17:23:36 +0100

    cleaning

--- ML/main.py
+++ ML/main.py
@@ -7,8 +7,9 @@ from flask import Flask, request
 app = Flask(__name__)
 CORS(app)
 
-def decode_base64_to_wav(base64_string):
-    # remove the header information from the base64 string
+
+# Transformer la donnée de la base64 au format mp3 et la stocker dans un fichier output.mp3
+def decode_base64_to_mp3(base64_string):
     wav_bytes = base64.b64decode(base64_string)
     
     # write the bytes to a file
@@ -23,8 +24,10 @@ def handle_request():
     print(data)
     text = ''
     base64_string = data['audioData']
-    decode_base64_to_wav(base64_string)
+    decode_base64_to_mp3(base64_string)
+    #  Lire le fichier mp3
     data, sr = librosa.load('output.mp3')
+    # Transformer l'audio mp3 au format wav
     sf.write("example.wav", data, sr)
 
     recognizer = speech_recognition.Recognizer()
@@ -32,17 +35,17 @@ def handle_request():
     with speech_recognition.AudioFile("example.wav") as wavaudio:
         audio = recognizer.record(wavaudio, duration=5)
         
-        # recognize speech using Google Speech Recognition
+        # reconnaître la parole à l'aide de Google Speech Recognition
         try:
             text = recognizer.recognize_google(audio, language='fr-FR')
             text = text.lower()
         except speech_recognition.UnknownValueError:
-            text = "Je n'ai pas compris votre audio, pouvez-vous le répéter"
+            text = "Je n'ai pas compris votre audio, pouvez-vous le répéter ?"
         except speech_recognition.RequestError as e:
-            text = "Could not request results from Google Speech Recognition service; {0}".format(e)
+            text = "Impossible de demander les résultats du service de reconnaissance vocale Google; {0}".format(e)
     return {
         'statusCode': 200,
-        'body': text
+        'body': text  # envoyer le text transcrit
     }
 
 
